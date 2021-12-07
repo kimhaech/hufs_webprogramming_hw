3d8a09124d77c68c48873ca2a0f78c53ecc976cc
kimhaech <epiclog@hufs.ac.kr>
2021-12-07 22:54:50 +0900

    change login

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
-const loginForm = document.getElementById("login-form")
-const loginInput = loginForm.querySelector("input")
-const greeting = document.querySelector("#greeting")
+const loginForm = document.getElementById("login-form") // 로그인 폼
+const loginInput = loginForm.querySelector("input") // 로그인 입력 창
+const greeting = document.querySelector("#greeting")  // 인사말
+const bucketlistInput = document.querySelector("#bucket-form")  // 버킷리스트 입력 창
 
+//
+const lid_check = document.querySelector("#ID") // 로그인 id 입력 div
+const check_id = document.querySelector("input")
+const lpw_check = document.querySelector("#PW") // 로그인 pw 입력 div
+const check_pw = document.querySelector("input")
+const loginButton = document.querySelector(".loginButton") // 로그인 버튼
+
+function CheckValue(){
+  const ID = check_id.value;  // id
+  const PASSWORD = check_pw.value;  // pw
+  if(localStorage.getItem(ID) == PASSWORD){ // localstorage에 입력한 id와 pw가 일치하는 경우
+    alert("SUCCESS")
+  }else{
+    alert("WRONG ID or PASSWORD")
+  }
+}
+
+loginButton.addEventListener("click", CheckValue) // 로그인 버튼 클릭시 함수 호출
+//
 const HIDDEN = "hidden"
 const USERNAME_KEY = "username"
 
@@ -22,7 +42,8 @@ if(savedUsername === null){ // 저장된 유저이름이 없다
   // login 띄움
   loginForm.classList.remove(HIDDEN)
   loginForm.addEventListener("submit",onLoginSubmit)
-}else{
+  }else{   
   greeting.innerText = `Hello ${savedUsername}`
   greeting.classList.remove(HIDDEN)
+  bucketlistInput.classList.remove(HIDDEN)
 }
